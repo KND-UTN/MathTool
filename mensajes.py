353af221f9994205f0d9744e59f1d3a6c3bd4a90
leispini <leilaspini15@gmail.com>
2020-06-24 03:34:40 -0300

    ya ta

--- mensajes.py
+++ mensajes.py
@@ -42,8 +42,8 @@ def opciones_tipo_b():
 def opciones_tipo_c():
     msg = 'Ha seleccionado Ecuacion Diferencial Ordinaria de Orden Superior con condiciones iniciales\n' \
           'Seleccione el metodo de resolucion\n' \
-          '1- \n' \
-          '2- \n' \
-          '3- \n'
+          '1- Metodo de Euler\n' \
+          '2- Metodo de Euler Mejorado\n' \
+          '3- Runge Kutta de Cuarto Orden\n'
     print(msg)
     return asegurar_rango(1, 3)
